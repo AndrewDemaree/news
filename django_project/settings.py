831b11aceaecbae144c384b7c4e918140ef5eeee
AndrewDemaree <andrew.demaree55@gmail.com>
2022-11-02 22:55:18 -0600

    email reset wont send do to lost connection, still having issues with article list not being visible as well as comments

--- django_project/settings.py
+++ django_project/settings.py
@@ -148,4 +148,10 @@ LOGOUT_REDIRECT_URL = "home"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # new
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "skate-the-bass@hotmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = "SSG.I1-djpMLSiyk15pZmhvvKw.qI4pphYu7LtAJAQE4DZt4kMEuLIyk7zkUJ12KiD7lLg"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
